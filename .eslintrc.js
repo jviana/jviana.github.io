7a7a72889363ddef8788d3b3cd456e13012ebee1
unknown <jose.viana.z@gmail.com>
2020-03-04 22:33:45 +0000

    C19 first step

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "semi"  : ["error", "always"],
+        "indent": ["error", 4],
+    },
+    "env": {
+        "jquery": true,
+        "browser": true
+    },
+};
