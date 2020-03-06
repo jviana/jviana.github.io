4ea2d4354d79670bdcf970decec41b2133a792cb
José Viana <jose.viana.z@gmail.com>
2020-03-06 17:00:01 +0000

    06Mar

--- c19/assets/c19.js
+++ c19/assets/c19.js
@@ -2,11 +2,20 @@ class C19 {
     constructor () {
         console.log('started...');
     }
-
+    getPercentage (total, value) {
+        let percentage;
+        percentage = value / total * 100;
+        percentage = percentage.toFixed(0);
+        return percentage;
+    }
     setBars (data) {
+        const self = this;
         let barContent;
         let total;
         total = 0;
+        $.each(data, function (i, d) {
+            total += d.ptConfirmed;
+        });
         $.each(data, function (i, d) {
             let date;
             let day;
@@ -15,6 +24,8 @@ class C19 {
             barContent = $('div#day-template').html();
             barContent = barContent.replace(/{{date}}/g, day);
             barContent = barContent.replace(/{{ptConfirmed}}/g, d.ptConfirmed);
+            barContent = barContent.replace(/{{percentage}}/g, 0);
+            barContent = barContent.replace(/{{percentage}}/g, self.getPercentage(total, d.ptConfirmed));
             total += d.ptConfirmed;
             $('div#main-content-bars').append(`${barContent}`);
         });
@@ -23,7 +34,7 @@ class C19 {
         console.log('chart started...');
     }
     getData () {
-        self = this;
+        const self = this;
         fetch('https://jviana.github.io/c19/data/c19.json')
         .then(response => response.json())
         .then(data => {
