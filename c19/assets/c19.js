a8551a3ecdcc1bf61fa233dc1a96e642c8d4836e
José Viana <jose.viana.z@gmail.com>
2020-03-16 14:25:33 +0000

    Mar16

--- c19/assets/c19.js
+++ c19/assets/c19.js
@@ -305,36 +305,53 @@ class C19 {
     }
 
     drawChart () {
-        var data = new google.visualization.DataTable();
+    var data = new google.visualization.DataTable();
       data.addColumn('string', 'Dia');
       data.addColumn('number', 'Confirmados');
       data.addColumn('number', 'Recuperados');
       data.addColumn('number', 'Óbitos');
 
-      /* data.addRows([
-        [1,  37.8, 80.8, 41.8],
-        [2,  30.9, 69.5, 32.4],
-        [3,  25.4,   57, 25.7],
-        [4,  11.7, 18.8, 10.5],
-        [5,  11.9, 17.6, 10.4],
-        [6,   8.8, 13.6,  7.7],
-        [7,   7.6, 12.3,  9.6],
-        [8,  12.3, 29.2, 10.6],
-        [9,  16.9, 42.9, 14.8],
-        [10, 12.8, 30.9, 11.6],
-        [11,  5.3,  7.9,  4.7],
-        [12,  6.6,  8.4,  5.2],
-        [13,  4.8,  6.3,  3.6],
-        [14,  4.2,  6.2,  3.4]
-      ]); */
-// console.log(c19.valuesForChart1);
       data.addRows(c19.valuesForChart1);
 
       var options = {
         title: 'Evolução diária',
+        titleTextStyle: {
+            color: '#455564',
+                fontSize: 14,
+                fontName: 'Open Sans Condensed',
+                bold: true,
+                italic: false
+        },
         colors: ['#17a2b8', '#28a745', '#6c757d'],
-        legend: { position: 'bottom' },
+        legend: { 
+            position: 'bottom',
+            textStyle: {
+                color: '#6c7587',
+                fontSize: 12,
+                fontName: 'Open Sans Condensed',
+                bold: false,
+                italic: false
+              }
+        },
         backgroundColor: 'transparent',
+        hAxis: {
+            textStyle: {
+              color: '#6c7587',
+              fontSize: 11,
+              fontName: 'Open Sans Condensed',
+              bold: false,
+              italic: false
+            }
+          },
+          vAxis: {
+            textStyle: {
+              color: '#6c7587',
+              fontSize: 11,
+              fontName: 'Open Sans Condensed',
+              bold: false,
+              italic: false
+            }
+          }
       };
 
       /* var chart = new google.charts.Line(document.getElementById('chart_absolute_daily_value'));
