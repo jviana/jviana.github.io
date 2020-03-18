b77234dea6e5f1c43e89cc04d2254aeee3994e89
unknown <jose.viana.z@gmail.com>
2020-03-18 12:30:49 +0000

    Mar18

--- c19/assets/c19.js
+++ c19/assets/c19.js
@@ -337,11 +337,11 @@ class C19 {
             if (valueActiveToBars <= 2) {
                 lastBarRendered.addClass('text-dark');
             }
-            if (valueActiveToBars === 1) {
+            /* if (valueActiveToBars === 1) {
                 lastBarRendered.addClass('pl-2');
             } else if (valueActiveToBars === 2) {
                 lastBarRendered.addClass('pl-3');
-            }
+            } */
             // adjust text (ptConfirmed) --END
             lastDate = date;
         });
